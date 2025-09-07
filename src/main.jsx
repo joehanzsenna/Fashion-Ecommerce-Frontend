9b0428430641635bf81123c11715802f809bbd2b
joehanzsenna <jonathanedison34@gmail.com>
2025-09-07 20:12:47 +0100

    adjusted layout structure on zoom out

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import ShopContextProvider from './context/ShopContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./context/ShopContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ShopContextProvider>
-      <App />
+      <div className="max-w-[1440px] w-[100%] m-[auto]">
+        <App />
+      </div>
     </ShopContextProvider>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
